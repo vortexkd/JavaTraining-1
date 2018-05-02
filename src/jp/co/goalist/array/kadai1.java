e89416f2592ad03d45061e6cbc518a2425749511
vortexkd <vortexkd@users.noreply.github.com>
2018-05-02 11:51:19 +0900

    ちょっと綺麗にしました^^
    進みが速いですね！すごい！
    配列の全てのエレメントに何かしたい時にこの形の "for each"ループも使えますよ、効率はちょっと上がり、とても読みやすくなると思います！
    後、この課題には"1 * 10 = 10"のように、スペースが入ってるので、気をつけてね ^^
    これからの難しい課題までもソースコードを見るのが楽しみにしてます！

--- src/jp/co/goalist/array/kadai1.java
+++ src/jp/co/goalist/array/kadai1.java
@@ -6,9 +6,9 @@ public class kadai1 {
         int[] ary1 = {1, 2, 3, 4, 5};
         int[] ary2 = {10, 20, 30, 40, 50};
         
-        for(int x=0;x<ary1.length;x++){
-            for(int y=0;y<ary2.length;y++){
-                System.out.println(ary1[x]+"*"+ary2[y]+"="+ary1[x]*ary2[y]);
+        for (int x: ary1) {
+            for (int y: ary2) {
+                System.out.println(x + " * " + y + " = " + (x * y));
             }
         }
     }
